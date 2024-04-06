5fb2c74325cce054aec470905cec0ea95441005f
Christopher Acosta <chriacos@espol.edu.ec>
2024-04-06 18:31:53 -0500

    Imagen movida a public

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1>Hello World desde React!</h1>
-      <img src='src\assets\codingsacademy.png' alt="Codings Academy" width="200" />
+      <img src='./codingsacademy.png' alt="Codings Academy" width="200" />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum, fugiat eveniet veniam corporis consequatur facilis fuga dolorem quos architecto exercitationem cumque voluptas commodi, rerum officia molestiae voluptatibus, delectus explicabo!</p>
     </>
   )
